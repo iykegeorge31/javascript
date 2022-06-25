58e6dd947c7c6599f758d6889a7bf99254f5811d
George Ekwebelem <ekwebelemgeorge@gmail.com>
2022-06-25 09:33:24 +0100

    Add files via upload

---
+++ javascript-basis.js
@@ -0,0 +1,13 @@
+// to print Name, Height and Country
+
+// One method
+
+console.log('Name: Ekwebelem George');
+console.log('Height: 6feet, 2inches');
+console.log('Country: Nigeria');
+
+// Another method
+
+alert("Name: Ekwebelem George");
+alert("Height: 6feet, 2inches");
+alert("Country: Nigeria");
